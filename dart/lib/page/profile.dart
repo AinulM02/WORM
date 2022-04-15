6aca29f9e182c6f9aa39ccc26ebcbb3e68a46950
AinulM02 <janulm02@gmail.com>
2022-04-15 20:18:37 +0700

    profile

--- dart/lib/page/profile.dart
+++ dart/lib/page/profile.dart
@@ -7,20 +7,125 @@ class profile extends StatefulWidget {
   State<profile> createState() => _profileState();
 }
 
+// ignore: camel_case_types
 class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [
+        children: <Widget>[
           Container(
-            height: 200,
-            color: Colors.amber,
-            // decoration: BoxDecoration(
-            //     image: const DecorationImage(
-            //   image: AssetImage('images/fix_profile.png'),
-            // )),
+            height: 270,
+            decoration: const BoxDecoration(
+              // borderRadius: BorderRadius.only(
+              //   bottomLeft: Radius.elliptical(45, 15) ,
+              //   bottomRight: Radius.elliptical(45, 15),
+              // ),
+              gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 31, 31, 31), Color.fromARGB(214, 10, 10, 10)],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: const Text(
+                    "Data Profile",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      ),
+                    ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      minRadius: 66,
+                      child: CircleAvatar(
+                        radius: 65,
+                        backgroundImage: AssetImage("assets/images/Aan.jpg"),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20, ) ,
+                  width: 150,
+                  height: 25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Ainul Muhlasin",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                ),
+
+              ],
+            ),
           ),
+
+          Container(
+            margin: const EdgeInsets.only(top: 16,right: 16, left: 16),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 2,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+              child: FlatButton(
+                child: Text('FAQ'),
+                color: Colors.white,
+                textColor: Colors.black,
+                onPressed: () {},
+              ),
+          ),
+
+          Container(
+            margin: const EdgeInsets.only(top: 10, right: 16, left: 16),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 2,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+            child: FlatButton(
+              child: Text('About'),
+              color: Colors.white,
+              textColor: Colors.black,
+              onPressed: () {},
+            ),
+          ),
+
         ],
       ),
     );
