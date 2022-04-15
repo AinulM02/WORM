b65fbcc406da3817746ccbcb0184627e36314a15
AinulM02 <janulm02@gmail.com>
2022-04-15 18:41:13 +0700

    jadwal-home

--- dart/lib/page/myHomePage.dart
+++ dart/lib/page/myHomePage.dart
@@ -58,88 +58,119 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 10,
           ),
           Container(
-            margin: const EdgeInsets.only(right: 16, left: 16, ),
-            height: 50,
-
-            color: Colors.white,
-            alignment: Alignment.center,
-            child: const Text("Jadwal Kegiatan"),
-          ),
-          Container(
-            margin: const EdgeInsets.only(right: 16, left: 16, bottom: 16),
-            height: 450,
-            color: Colors.white,
-            child: ListView(
-              children: const [
-                ListTile(
-                  title: Text("Progres 1"),
-                  subtitle: Text(
-                    "Progres sudah selesai",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  trailing: Text("10.00 PM"),
-                ),
-                Divider(
+            margin: const EdgeInsets.only(right: 16, left: 16),
+            height: 480,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(3),
+              // ignore: prefer_const_literals_to_create_immutables
+              boxShadow: [
+                const BoxShadow(
                   color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text("Progres 2"),
-                  subtitle: Text(
-                    "Progres sudah selesai",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  trailing: Text("10.00 PM"),
-                ),
-                Divider(
-                  color: Colors.grey,
-                ),
-                ListTile(
-                  title: Text("Progres 3"),
-                  subtitle: Text(
-                    "Progres sudah selesai",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  trailing: Text("10.00 PM"),
-                ),
-                Divider(
-                  color: Colors.grey,
+                  blurRadius: 2,
+                  offset: Offset(0, 0), // Shadow position
                 ),
               ],
             ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 18),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Jadwal Kegiatan",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 24, 24, 24),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const ListTile(
+                  title: Text("Progres 1"),
+                  subtitle: Text(
+                    "Progres sudah selesai",
+                  ),
+                  trailing: Text("10.00 PM"),
+                ),
+                const Divider(),
+                const ListTile(
+                  title: Text("Progres 2"),
+                  subtitle: Text(
+                    "Progres sudah selesai",
+                  ),
+                  trailing: Text("11.00 PM"),
+                ),
+                const Divider(),
+                const ListTile(
+                  title: Text("Progres 3"),
+                  subtitle: Text(
+                    "Progres sudah selesai",
+                  ),
+                  trailing: Text("12.00 PM"),
+                ),
+                const Divider(),
+                const ListTile(
+                  title: Text("Progres 4"),
+                  subtitle: Text(
+                    "Progres sudah selesai",
+                  ),
+                  trailing: Text("12.00 PM"),
+                ),
+                const Divider(),
+              ],
+            ),
           ),
+          // Container(
+            
+          //   margin: const EdgeInsets.only(right: 16, left: 16, bottom: 16),
+          //   height: 450,
+          //   color: Colors.white,
+          //   child: ListView(
+              
+          //     children: const [
+          //       ListTile(
+          //         title: Text("Progres 1"),
+          //         subtitle: Text(
+          //           "Progres sudah selesai",
+          //           maxLines: 2,
+          //           overflow: TextOverflow.ellipsis,
+          //         ),
+          //         trailing: Text("10.00 PM"),
+          //       ),
+          //       Divider(
+          //         color: Colors.grey,
+          //       ),
+          //       ListTile(
+          //         title: Text("Progres 2"),
+          //         subtitle: Text(
+          //           "Progres sudah selesai",
+          //           maxLines: 2,
+          //           overflow: TextOverflow.ellipsis,
+          //         ),
+          //         trailing: Text("10.00 PM"),
+          //       ),
+          //       Divider(
+          //         color: Colors.grey,
+          //       ),
+          //       ListTile(
+          //         title: Text("Progres 3"),
+          //         subtitle: Text(
+          //           "Progres sudah selesai",
+          //           maxLines: 2,
+          //           overflow: TextOverflow.ellipsis,
+          //         ),
+          //         trailing: Text("10.00 PM"),
+          //       ),
+          //       Divider(
+          //         color: Colors.grey,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+
         ],
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   type: BottomNavigationBarType.fixed,
-      //   backgroundColor: Colors.blue,
-      //   selectedItemColor: Colors.white,
-      //   unselectedItemColor: Colors.amber,
-      //   // selectedFontSize: 15,
-      //   currentIndex: currentIndex,
-      //   // showUnselectedLabels: false,
-      //   onTap: (index) => setState(() => currentIndex = index),
-      //   items: const [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.schedule),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.payment),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.person),
-      //       label: 'Home',
-      //     ),
-      //   ],
-      // ),
+
     );
   }
 }
