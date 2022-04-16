fdb3286b9a166d62ec1a9516bb72182ac4400994
AinulM02 <janulm02@gmail.com>
2022-04-16 17:18:10 +0700

    Merge branch 'master' of https://github.com/AinulM02/worm

--- dart/lib/page/jadwalPage.dart
+++ dart/lib/page/jadwalPage.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:getwidget/getwidget.dart';
-=======
+
 import 'package:worm/page/tambahJadwal.dart';
->>>>>>> 87a94ba655b7c1c58e1c499b93a0a6cf41334a6e
+
 
 class jadwalPage extends StatefulWidget {
   const jadwalPage({Key? key}) : super(key: key);
@@ -18,43 +16,7 @@ class _jadwalPageState extends State<jadwalPage> {
     return Scaffold(
       body: Column(children: <Widget>[
         const Padding(padding: EdgeInsets.all(18.0)),
-        Container(
-          margin: const EdgeInsets.only(right: 16, left: 16),
-          height: 80,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 2,
-                offset: Offset(0, 0), // Shadow position
-              ),
-            ],
-          ),
-          child: Column(
-            children: <Widget>[
-              const ListTile(
-                title: Text(
-                  "Progress",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 15,color: Colors.black, fontWeight: FontWeight.normal),
-                  ),
-                  trailing: Text(
-                    "80%",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-              ),
-              GFProgressBar(
-                margin: const EdgeInsets.only(right: 16, left: 16),
-                percentage: 0.8,
-                lineHeight: 10,
-                backgroundColor: const Color.fromARGB(255, 209, 209, 209),
-                progressBarColor: const Color.fromRGBO(254, 204, 118, 1),
-              )
-            ],
-          ),    
-        ),
+        
         const Padding(padding: EdgeInsets.only(top: 10)),
         Container(
           margin: const EdgeInsets.only(right: 16, left: 16),
