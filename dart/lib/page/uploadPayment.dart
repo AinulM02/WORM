30e82354a511e3ab784d33ea180b181aa314ee09
efriza <Efrizals@it.student.pens.ac.id>
2022-04-22 09:51:55 +0700

    febi ningkatno tampilan page upload_payment

--- dart/lib/page/uploadPayment.dart
+++ dart/lib/page/uploadPayment.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:date_time_picker/date_time_picker.dart';
+
 
 class uploadPayment extends StatefulWidget {
   const uploadPayment({Key? key}) : super(key: key);
@@ -8,53 +10,92 @@ class uploadPayment extends StatefulWidget {
 }
 
 class _uploadPayment extends State<uploadPayment> {
+
+  TimeOfDay time = TimeOfDay.now();
+  void showTime() {
+    showTimePicker(context: context, initialTime: TimeOfDay.now())
+        .then((value) {
+      setState(() {
+        time = value!;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // final args = ModalRoute.of(context)!.settings.arguments as int;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Upload Pembayaran",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 17,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+
+          title: const Text(
+            "Upload Pembayaran",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          backgroundColor: const Color.fromRGBO(246, 246, 246, 1),
         ),
-        backgroundColor: const Color.fromRGBO(246, 246, 246, 1),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(10.0),
-        child: new Column(
-          children: <Widget>[
-            new TextField(
-              decoration: new InputDecoration(
-                  hintText: "Masukkan Nominal",
-                  labelText: "Minimal Pembayaran",
-                  border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(20.0))),
+        body: ListView(children: [
+          Container(
+            margin: const EdgeInsets.only(top: 30, right: 16, left: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Nominal Pembayaran",
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                )
+              ],
             ),
-            new Padding(
-              padding: new EdgeInsets.only(top: 20.0),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, right: 16, left: 16),
+            child: DateTimePicker(
+              dateMask: 'd MMM, yyyy',
+              initialValue: DateTime.now().toString(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2100),
+              icon: const Icon(Icons.event),
+              dateLabelText: 'Tanggal Pembayaran',
+              selectableDayPredicate: (date) {
+                // Disable weekend days to select from the calendar
+                if (date.weekday == 6 || date.weekday == 7) {
+                  return false;
+                }
+                return true;
+              },
+              onChanged: (val) => print(val),
+              validator: (val) {
+                print(val);
+                return null;
+              },
+              onSaved: (val) => print(val),
             ),
-            new TextField(
-              decoration: new InputDecoration(
-                  labelText: "Tanggal Pembayaran",
-                  border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(20.0))),
+          ),
+
+          new Padding(
+            padding: new EdgeInsets.only(top: 20.0),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, right: 16, left: 16),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: showTime,
+                  icon: const Icon(Icons.timer),
+                ),
+                Text(time.format(context).toString()),
+              ],
             ),
-            new Padding(
-              padding: new EdgeInsets.only(top: 20.0),
-            ),
-            new TextField(
-              decoration: new InputDecoration(
-                  labelText: "Waktu Pembayaran",
-                  border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(20.0))),
-            ),
-          ],
-        ),
-      ),
-    );
+          )
+        ]));
   }
 }
